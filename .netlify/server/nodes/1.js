450b142bf8720564b22faf636b7073b903bcb1a5
Ester <91192024+ester-t-s@users.noreply.github.com>
2024-02-29 14:55:28 +0100

    delete netlify folder

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.bf050123.mjs';
-export const imports = ["_app/immutable/entry/error.svelte.bf050123.mjs","_app/immutable/chunks/index.1f10dd1a.mjs","_app/immutable/chunks/singletons.34f2d98f.mjs","_app/immutable/chunks/index.3b39a4aa.mjs"];
-export const stylesheets = [];
-export const fonts = [];
