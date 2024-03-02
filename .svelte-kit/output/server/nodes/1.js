8e397ef8524b5fd4461530152dba6d8935e7ee4a
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2024-03-02 15:05:00 +0100

    remove .svelte-kit from repo (these files are locally built)

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.d40c74ac.js","_app/immutable/chunks/index.c6f817c9.js","_app/immutable/chunks/singletons.4dc45195.js","_app/immutable/chunks/index.cb7dbe23.js"];
-export const stylesheets = [];
-export const fonts = [];
