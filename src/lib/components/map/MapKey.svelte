bf26d46c45a3b3b11127ec08fa2aa1a3533e11a9
Ester Tamara Scheck <91192024+ester-t-s@users.noreply.github.com>
2024-03-14 14:35:36 +0100

    fix(mapkey): add stroke to legend text

--- src/lib/components/map/MapKey.svelte
+++ src/lib/components/map/MapKey.svelte
@@ -16,9 +16,10 @@
     <div class="inline-block align-middle">
       <div
         class="w-4 h-4 rounded-full ml-2 inline-block"
-        style={`background-color:${color}; border: 0.5px solid white;`}
+        style={`background-color:${color}; border: 1.5px solid white;`}
       />
-      <p class="align-middle leading-4 ml-1 inline-block mb-2" style="text-shadow: 0 0 2px #fff;">
+      <p class="align-middle leading-4 ml-1 inline-block mb-2" style="text-shadow: 2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff,
+      1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;;">
         {$lang === "de" ? name : name_en}
       </p>
     </div>
