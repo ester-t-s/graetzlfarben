2c5d1f938dd253c77a13ac9d2a5d39becbc74717
Ester <91192024+ester-t-s@users.noreply.github.com>
2024-03-19 17:22:31 +0100

    clean up: delete depricated asset and commented out code

--- src/lib/assets/base64.js
+++ src/lib/assets/base64.js
@@ -42,50 +42,6 @@ export function encode(input) {
   return output;
 }
 
-// public method for decoding
-
-// export function decode (input) {
-//   var output = "";
-
-//   var chr1, chr2, chr3;
-
-//   var enc1, enc2, enc3, enc4;
-
-//   var i = 0;
-
-//   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-//   while (i < input.length) {
-//     enc1 = this._keyStr.indexOf(input.charAt(i++));
-
-//     enc2 = this._keyStr.indexOf(input.charAt(i++));
-
-//     enc3 = this._keyStr.indexOf(input.charAt(i++));
-
-//     enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-//     chr1 = (enc1 << 2) | (enc2 >> 4);
-
-//     chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-
-//     chr3 = ((enc3 & 3) << 6) | enc4;
-
-//     output = output + String.fromCharCode(chr1);
-
-//     if (enc3 != 64) {
-//       output = output + String.fromCharCode(chr2);
-//     }
-
-//     if (enc4 != 64) {
-//       output = output + String.fromCharCode(chr3);
-//     }
-//   }
-
-//   output = _utf8_decode(output);
-
-//   return output;
-// }
-
 // private method for UTF-8 encoding
 function _utf8_encode(string) {
   string = string.replace(/\r\n/g, "\n");
