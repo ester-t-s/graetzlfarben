9239766b0d73f8be1b42746090d572e0808a6c6a
Ester Tamara Scheck <91192024+ester-t-s@users.noreply.github.com>
2024-03-27 16:19:54 +0100

    fix(PostcardFront): update subtitle

--- src/lib/components/postcard/PostcardFront.svelte
+++ src/lib/components/postcard/PostcardFront.svelte
@@ -198,7 +198,7 @@ https://observablehq.com/@d3/treemap
       .attr("font-family", "IBM Plex Sans Bold")
       .attr("font-size", 10)
       .attr("fill", "#2f2fa2")
-      .text("TU Wien GEO-Tag 2024 — cartolab.at/gf");
+      .text("Geo-Tag 2024 — cartolab.at/graetzlfarben");
   }
 
   function updateData(newData) {
