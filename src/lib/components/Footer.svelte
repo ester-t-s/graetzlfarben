3d34d375cc874af8c2e2a252f578f41fefe2ea3d
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2024-04-10 15:54:24 +0200

    change repo link and add link to carto group

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,15 +9,16 @@
 
 <div class=" text-sm text-gray-400 mt-4">
   <p>
-    {$lang === "en"
-      ? "Grätzlfarben was initially developed under the name 'Kiezcolors' by ODIS and CityLAB Berlin. The research unit cartography from TU Wien adapted it for Vienna. You can find the code to this project on "
-      : "Grätzlfarben wurde ursprünglich unter dem Namen 'Kiezcolors' von ODIS und dem CityLAB Berlin entwickelt. Die Forschungsgruppe Kartographie der TU Wien hat das Projekt für Wien weiterentwickelt. Den Code zum Projekt findest auf "}
+    {@html $lang === "en"
+      ? "Grätzlfarben was initially developed under the name 'Kiezcolors' by ODIS and CityLAB Berlin. The <a href='https://cartography.tuwien.ac.at/' class='underline' target='_blank'>research unit cartography</a> at TU Wien adapted it for Vienna. You can find the code to this project on "
+      : "Grätzlfarben wurde ursprünglich unter dem Namen 'Kiezcolors' von ODIS und dem CityLAB Berlin entwickelt. Die <a href='https://cartography.tuwien.ac.at/' class='underline' target='_blank'>Forschungsgruppe Kartographie</a> der TU Wien hat das Projekt für Wien weiterentwickelt. Den Code zum Projekt findest auf "}
     <a
-      class="underline"
-      href="https://github.com/technologiestiftung/kiezcolors/"
+      class="underline" 
+      target="_blank"
+      href="https://github.com/ester-t-s/graetzlfarben"
     >
       GitHub.
-    </a><!-- TODO update github link-->
+    </a>
     
     <br /><br />
 
@@ -45,7 +46,7 @@
     Impressum:
     <br>
     Dies ist ein Forschungsprototyp – Daten und Funktionalität ohne Gewähr! 
-    Entwickelt am Forschungsbereich Kartographie<br>
+    Entwickelt am <a href="https://cartography.tuwien.ac.at/" class="underline" target="_blank">Forschungsbereich Kartographie</a><br>
     TU Wien, Wiedner Hauptstraße 8-10, 1040 Wien
     <!--
     <input
