519a5e09613d486110ea50611eeff36fcd58ca0f
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2024-07-16 11:58:59 +0200

    tweak layout

--- src/components/postcard/PostcardFront.svelte
+++ src/components/postcard/PostcardFront.svelte
@@ -141,7 +141,7 @@ https://observablehq.com/@d3/treemap
       .enter()
       .append("text")
       .attr("class", "title-text")
-      .attr("transform", "translate(" + width / 2 + "," + (height * 0.91 - postcardMargin) + ")")
+      .attr("transform", "translate(" + width / 2 + "," + (height * 0.93 - postcardMargin) + ")")
       .attr("text-anchor", "middle")
       .attr("font-family", "IBM Plex Sans Bold")
       .attr("font-size", 30)
